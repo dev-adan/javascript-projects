1bfd0fe3373727d0c4c188269844c0a8738b995e
adan <adanaligakhar@gmail.com>
2022-05-04 17:38:47 +0500

    hidden search widget project

---
+++ 4.Hidden search button/app.js
@@ -0,0 +1,20 @@
+const search = document.querySelector('.search');
+const button = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+
+button.addEventListener('click', () => {
+
+    if(search.classList.contains('active')){
+        search.classList.remove('active');
+
+    }else{
+         search.classList.add('active');
+         input.focus();
+    }
+
+   
+})
+
+
+
